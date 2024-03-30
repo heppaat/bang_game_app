b19ce5b851eace1f91f3545564c70815f2c0a357
heppaat <pali.herrer@gmail.com>
2024-03-30 18:37:28 +0100

    login completed

---
+++ frontend/src/components/Game.tsx
@@ -0,0 +1,9 @@
+const Game = () => {
+  return (
+    <div className="flex justify-center">
+      <div className="loading loading-spinner loading-lg"></div>
+    </div>
+  );
+};
+
+export default Game;
